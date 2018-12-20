c2f5bf6e095840b4b9fff58a7e97cec0befbf9c5
Joshua Whiting <inejwstine@gmail.com>
2018-12-20 13:39:36 -0700

    initial setup of ajax call

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,23 @@ function initMap() {
         map: map
     });
 }
+
+function displayWeather(data){
+    console.log("success", data);
+}
+
+
+$(document).ready(function() {
+    $("#map").hide(0).fadeIn(1000);
+    
+    var lat = -25.344;
+    var lon = 131.036;
+    $.ajax({
+        type: 'GET',
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + WEATHER_APPID,
+        //url: "http://api.openweathermap.org/data/2.5/weather?lat=-25.344&lon=131.036&appid=15473ff2f5108c77c45e3cd4e64cf6e6",
+        success: function(data){
+            console.log("success", data);
+        }
+    })
+});
