1af850e7c2007cf16ed043ed4df15de67446628a
Joshua Whiting <inejwstine@gmail.com>
2018-12-20 19:02:17 -0700

    retrieves weather data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
+
+// Add your weather app id here
+//var WEATHER_APPID = "";
+
 function initMap() {
     var uluru = {lat: -25.344, lng: 131.036};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -27,6 +31,14 @@ $(document).ready(function() {
         //url: "http://api.openweathermap.org/data/2.5/weather?lat=-25.344&lon=131.036&appid=15473ff2f5108c77c45e3cd4e64cf6e6",
         success: function(data){
             console.log("success", data);
+            $('#weather-location').html("x-location");
+            $('#weather-condition').html(data.weather[0].main);
+            $('#weather-temperature').html(data.main.temp);
+            $('#weather-humidity').html(data.main.humidity);
+            $('#weather-sunrise').html(data.sys.sunrise);
+            $('#weather-sunset').html(data.sys.sunset);
+            $('#weather-windspeed').html(data.wind.speed);
+            $('#weather-winddirection').html(data.wind.deg);
         }
     })
 });
